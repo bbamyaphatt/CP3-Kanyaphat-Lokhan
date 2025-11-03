6330da39c5a195bfd4c819234c5d90dee6afcd71
bbamyaphatt <L.kanyaphatt@gmail.com>
2025-11-03 12:47:14 +0700

    Create script to input and display scores for subjects
    This script takes input scores for four subjects and prints them.

---
+++ Exercise4_Kanyaphat_L.py
@@ -0,0 +1,12 @@
+# input
+FE = float(input("Foundation English Score: "))
+GB = float(input("General Business Score: "))
+IC = float(input("Introduction to Computer Systems Score: "))
+CP = float(input("Computer Programming Score: "))
+
+# output
+print("---------- Your Score ----------")
+print("Foundation English Score: ", FE)
+print("General Business Score: ", GB)
+print("Introduction to Computer Systems Score: ", IC)
+print("Computer Programming Score: ", CP)
